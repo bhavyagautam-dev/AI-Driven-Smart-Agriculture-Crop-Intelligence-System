6522fa30368830d68e00676256f2bada69ada0d2
bhavyagautam-dev <bhavya.gautam_cs.aiml24@gla.ac.in>
2026-03-07 17:33:08 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,13 +1,24 @@
 import streamlit as st
 from iot.sensor_simulation import get_sensor_data
 from src.irrigation import irrigation_advice
+from src.crop_prediction import recommend_crop
 
-st.title("Smart Agriculture IoT Dashboard")
+st.title("AI Smart Agriculture IoT System")
 
-data = get_sensor_data()
+sensor_data = get_sensor_data()
 
-st.write("Sensor Data", data)
+st.write("Sensor Data", sensor_data)
 
-advice = irrigation_advice(data["soil_moisture"])
+crop = recommend_crop(
+    90,40,40,
+    sensor_data["temperature"],
+    sensor_data["humidity"],
+    sensor_data["ph"],
+    120
+)
 
-st.write("Irrigation Advice:", advice)
+st.write("Recommended Crop:", crop)
+
+irrigation = irrigation_advice(sensor_data["soil_moisture"])
+
+st.write("Irrigation Advice:", irrigation)
