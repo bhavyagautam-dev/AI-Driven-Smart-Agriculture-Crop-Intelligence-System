5859040cc538334119ec3b0a6db106cacb190250
bhavyagautam-dev <bhavya.gautam_cs.aiml24@gla.ac.in>
2026-03-07 17:36:14 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -3,22 +3,28 @@ from iot.sensor_simulation import get_sensor_data
 from src.irrigation import irrigation_advice
 from src.crop_prediction import recommend_crop
 
-st.title("AI Smart Agriculture IoT System")
+st.title("🌱 AI Smart Agriculture IoT System")
+
+st.header("Sensor Data")
 
 sensor_data = get_sensor_data()
 
-st.write("Sensor Data", sensor_data)
+st.write(sensor_data)
+
+st.header("AI Crop Recommendation")
 
 crop = recommend_crop(
-    90,40,40,
+    90, 40, 40,
     sensor_data["temperature"],
     sensor_data["humidity"],
     sensor_data["ph"],
     120
 )
 
-st.write("Recommended Crop:", crop)
+st.success(f"Recommended Crop: {crop}")
+
+st.header("Smart Irrigation Advice")
 
 irrigation = irrigation_advice(sensor_data["soil_moisture"])
 
-st.write("Irrigation Advice:", irrigation)
+st.info(irrigation)
